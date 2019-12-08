8184be52da818fed2ccb8d9d7840273057bfc938
dmitriy <logart1995@gmail.com>
2019-12-08 18:03:07 +0200

    Try to reload app after getting new data

--- clock.py
+++ clock.py
@@ -7,11 +7,16 @@ Created on Tue Feb 15 18:22:53 2019.
 """
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
+import signal
+import os
 
-cmd ="python3 itcfinally2.py"
-subprocess.call(cmd, shell=True)
+# cmd ="python3 itcfinally2.py"
+# subprocess.call(cmd, shell=True)
 cmd = "python3 app.py"
-subprocess.call(cmd, shell=True)
+print("Open process for a Dash app")
+dash_app_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+
+
 
 
 sched = BlockingScheduler(timezone="Europe/Kiev")
@@ -33,12 +38,18 @@ def timed_job():
 # del job
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=20, minute=00)
-def scheduled_job():
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18, minute=00)
+def scheduled_job(process=dash_app_process):
     """Schedule a job."""
     print('This job is run every weekday at 5pm.')
+    print('Kill process of a Dash app')
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
     cmd = "python3 itcfinally2.py"
-    subprocess.call(cmd, shell=False)
+    subprocess.call(cmd, shell=True)
+    print('Create a new process of a Dash app')
+    cmd = "python3 app.py"
+    global dash_app_process
+    dash_app_process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
 
 
 sched.start()
